20190945fc00a9e057d57fa101217a7d96ff1f35
amir dwikat <amirdwikat@gmail.com>
2025-03-20 14:16:56 +0300

    Making Models

--- django-store/store/models.py
+++ django-store/store/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    featured = models.BooleanField(default=False)
+    order = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255, null=True)
+    description = models.TextField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(null=True)
+    image = models.ImageField(null=True)
+    featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
+
+class Order(models.Model):
+    customer = models.JSONField(default=dict)
+    total = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Slider(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.TextField(max_length=500)
+    image = models.ImageField(null=True)
+    order = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
