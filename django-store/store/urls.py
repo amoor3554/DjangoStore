68eaaa7dc43da07f6f153603894708ca3c153313
amir dwikat <amirdwikat@gmail.com>
2025-03-20 17:48:52 +0300

    Templates Added

--- django-store/store/urls.py
+++ django-store/store/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from. import views
 
 urlpatterns = [
-    
+    path('', views.index, name='StoreHome'),
+    path('product/<int:pid>', views.product, name='SroreProduct'),
+    path('category/<int:cid>', views.category, name='StoreCategory'),
+    path('category/', views.category, name='StoreCategory'),
+    path('cart/', views.cart, name='StoreCart'),
+    path('checkout/', views.check_out, name='StoreCheckout'),
+    path('checkout/complete/', views.check_out_complete, name='StoreCheckoutComplete'),
 ]
--- django-store/store/views.py
+++ django-store/store/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from .models import Product, Slider
 
-# Create your views here.
+def index(request):
+    product = Product.objects.select_related('author').filter(featured=True)
+    slider = Slider.objects.order_by('order')
+    return render(
+        request, 'index.html',
+        {
+            'product': product,
+            'slider': slider,
+        }
+    )
+
+
+def product(request, pid):
+    return render(
+        request, 'product.html'
+    )
+
+
+def category(request, cid=None):
+    return render(
+        request, 'category.html'
+    )
+
+
+def cart(request):
+    return render(
+        request, 'cart.html'
+    )
+
+
+def check_out(request):
+    return render(
+        request, 'check_out.html'
+    )
+
+
+def check_out_complete(request):
+    return render(
+        request, 'check_out_complete.html'
+    )
